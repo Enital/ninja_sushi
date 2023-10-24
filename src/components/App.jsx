bd8c8e6f1979f95815ae965673841348ba0372d9
Enital <117684114+Enital@users.noreply.github.com>
2023-10-24 16:51:43 +0300

    add Header,  Categories pages

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,18 @@
+import CategoriesPage from 'pages/CategoriesPage/CategoriesPage';
 import MainPage from 'pages/MainPage/MainPage';
+import { Routes, Route, Navigate } from 'react-router-dom';
+import Layout from './Layout/Layout';
 
 export const App = () => {
   return (
-    <div>
-      <MainPage />
-    </div>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<MainPage />} />
+        <Route path="categories" element={<CategoriesPage />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Route>
+
+      {/* <MainPage /> */}
+    </Routes>
   );
 };
