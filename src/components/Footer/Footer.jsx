651c05b9a0b018be06cb8a196c3e13143d54e33f
Enital <117684114+Enital@users.noreply.github.com>
2023-10-26 21:21:57 +0300

    add footer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,3 @@
+export default function Footer() {
+  return <div className={css.footer}>Footer</div>;
+}
--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -1,10 +1,12 @@
 import Card from 'components/Card/Card';
+import Footer from 'components/Footer/Footer';
 
 export default function MainPage() {
   return (
     <div>
       <h1>MainPage</h1>
       <Card />
+      <Footer />
     </div>
   );
 }
