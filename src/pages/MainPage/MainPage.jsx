88b826d9441a13552090f9f57ac34cb9ea1efb65
Enital <117684114+Enital@users.noreply.github.com>
2023-10-27 20:18:00 +0300

    Update MainPage.jsx

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -1,11 +1,11 @@
-import Card from 'components/Card/Card';
+import FoodCategories from 'components/FoodCategories/FoodCategories';
 import Footer from 'components/Footer/Footer';
 
 export default function MainPage() {
   return (
     <div>
       <h1>MainPage</h1>
-      <Card />
+      <FoodCategories />
       <Footer />
     </div>
   );
