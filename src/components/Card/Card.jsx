bb3d77fec787d10263c78efbc174124ab560ff34
Enital <117684114+Enital@users.noreply.github.com>
2023-10-29 15:15:48 +0200

    update Card& Card styles

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -2,8 +2,8 @@ import heart from '../../images/icons/heart.svg';
 import plus from '../../images/icons/plus.svg';
 import chilli from '../../images/icons/chilli.svg';
 import nature from '../../images/icons/nature.svg';
-import lactoseFree from '../../images/icons/lactoseFree.svg';
-import not from '../../images/icons/not.svg';
+import lactose from '../../images/icons/lactose.svg';
+// import not from '../../images/icons/not.svg';
 
 import css from './card.module.css';
 
@@ -12,15 +12,18 @@ export default function Card(item) {
   const isHit = data.hit;
   const isNew = data.new;
   const isChili = data.chili;
-  const isNature = data.spicy;
+  const isNature = true;
+  // const isNature = data.spicy;
   const isLactoseFree = true;
   console.log(data);
   // console.log(item.new);
   return (
     <div className={css.card}>
       <div className={css.imgBlock}>
-        {isHit && <p className={css.hit}>Hit</p>}
-        {isNew && <p className={css.new}>New</p>}
+        <ul className={css.hitNew}>
+          {isHit && <p className={css.hit}>Hit</p>}
+          {isNew && <p className={css.new}>New</p>}
+        </ul>
 
         <img src={data.img} alt="sushi" className={css.img} />
 
@@ -36,20 +39,22 @@ export default function Card(item) {
             </li>
           )}
           {isLactoseFree && (
-            <li className={css.lactoseFree}>
-              <img src={lactoseFree} alt="" className={css.lactose} />
-              <img src={not} alt="" className={css.not} />
+            <li className={css.lactose}>
+              <img src={lactose} alt="" className={css.lactose} />
             </li>
           )}
         </ul>
       </div>
 
-      <h2>Гункан Лосось</h2>
-      <p>Вес: 40г</p>
-      <p>
-        Нори, рис, японский майонез, бальзамик, трюфельная сальса, кунжутное
-        масло
-      </p>
+      <div className={css.description}>
+        <h2>Гункан Лосось</h2>
+        <p>Вес: 40г</p>
+        <p>
+          Нори, рис, японский майонез, бальзамик, трюфельная сальса, кунжутное
+          масло
+        </p>
+      </div>
+
       <ul className={css.bottomLine}>
         <p className={css.price}>
           190 <span className={css.currency}>грн</span>
