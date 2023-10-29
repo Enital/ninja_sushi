4efb865e20d5742ce5358fb5376d211500cbd82d
Enital <117684114+Enital@users.noreply.github.com>
2023-10-29 15:32:17 +0200

    Update FoodCategories.jsx

--- src/components/FoodCategories/FoodCategories.jsx
+++ src/components/FoodCategories/FoodCategories.jsx
@@ -1,14 +1,11 @@
-import Card from 'components/Card/Card';
-import items from './items.json';
+import Category from 'components/Card/Card';
+import sushi from './sushi.json';
 
 import css from './foodCategories.module.css';
 export default function FoodCategories() {
-  console.log(items);
   return (
     <div className={css.foodCategories}>
-      {items.map(item => (
-        <Card key={item.id} item={item} />
-      ))}
+      <Category items={sushi} />
     </div>
   );
 }
