6720f614a230074c5a35347094683c810c7df829
Enital <117684114+Enital@users.noreply.github.com>
2023-10-30 19:20:13 +0200

    CategoryBar update

--- src/components/CategoryBar/CategoryBar.jsx
+++ src/components/CategoryBar/CategoryBar.jsx
@@ -1,4 +1,51 @@
+import rollIcon from '../../images/icons/roll.svg';
+
 import css from './categoryBar.module.css';
 export default function CategoryBar() {
-  return <div className={css.bar}>CategoryBar</div>;
+  return (
+    <div className={css.bar}>
+      <ul className={css.barList}>
+        <li>
+          <a href="/#">
+            <img src={rollIcon} alt="rollIcon" />
+          </a>
+        </li>
+        <li>
+          <a href="/#">
+            <img src={rollIcon} alt="rollIcon" />
+          </a>
+        </li>
+        <li>
+          <a href="/#">
+            <img src={rollIcon} alt="rollIcon" />
+          </a>
+        </li>
+        <li>
+          <a href="/#">
+            <img src={rollIcon} alt="rollIcon" />
+          </a>
+        </li>
+        <li>
+          <a href="/#">
+            <img src={rollIcon} alt="rollIcon" />
+          </a>
+        </li>
+        <li>
+          <a href="/#">
+            <img src={rollIcon} alt="rollIcon" />
+          </a>
+        </li>
+        <li>
+          <a href="/#">
+            <img src={rollIcon} alt="rollIcon" />
+          </a>
+        </li>
+        <li>
+          <a href="/#">
+            <img src={rollIcon} alt="rollIcon" />
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
 }
