ee3764ca6f487115e84344a3b6f16d8bc62dea4f
Enital <117684114+Enital@users.noreply.github.com>
2023-11-02 20:42:01 +0200

    Update Header.jsx

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,3 +1,27 @@
 export default function Header() {
-  return <h1>Header</h1>;
+  return (
+    <div className={css.header}>
+      <h1>NINJA SUSHI</h1>
+      <ul>
+        <p>flag</p>
+        <p>Киев</p>
+        <p>RU</p>
+      </ul>
+      <ul>
+        <li>Главная</li>
+        <li>Доставка</li>
+        <li>О нас</li>
+        <li>Новости</li>
+        <li>
+          <p>tel</p>
+          <p>+380 00 000 00 00</p>
+        </li>
+      </ul>
+      <ul>
+        <li>
+          <img src="" alt="" />
+        </li>
+      </ul>
+    </div>
+  );
 }
