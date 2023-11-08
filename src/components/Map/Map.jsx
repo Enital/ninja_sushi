b5fbcca067a21880f06c4f9fec12394467785ce0
Enital <117684114+Enital@users.noreply.github.com>
2023-11-08 22:45:33 +0200

    Update Map.jsx

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,11 +1,29 @@
 import css from './map.module.css';
 export default function Map() {
   return (
-    <div className={css.map}>
-      <h3 className={css.mapTitle}>
-        Каждая кухня работает со своей зоной доставки, чтобы привезти еду
-        максимально быстро
-      </h3>
+    <div className={css.container}>
+      <div className={css.mapTitle}>
+        <h3 className={css.mapTitleText}>
+          Каждая кухня работает со своей зоной доставки, чтобы привезти еду
+          максимально быстро
+        </h3>
+        <ul>
+          <li>
+            <div className={css.green}></div>
+            <p> - Бесплатная доставка</p>
+          </li>
+          <li>
+            <div className={css.yellow}></div>
+            <p> - Платная доставка</p>
+          </li>
+          <li>
+            <div className={css.red}></div>
+            <p> - Доставку не делаем</p>
+          </li>
+        </ul>
+      </div>
+
+      <div className={css.map}></div>
     </div>
   );
 }
